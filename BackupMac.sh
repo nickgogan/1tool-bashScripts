58aff648c85789075649ebca806e88d75cb8d405
unknown <5131523+nickgogan@users.noreply.github.com>
2022-03-20 17:06:10 -0400

    Init.

---
+++ BackupMac.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh/
+
+# Copy dotfiles 
+cp ~/.zshrc ~/Desktop/Tech/Projects/Setup/Configs/nixUserHome
+cp ~/.zshenv ~/Desktop/Tech/Projects/Setup/Configs/nixUserHome
+cp -R ~/.zprofile ~/Desktop/Tech/Projects/Setup/Configs/nixUserHome
+cp -R ~/.zpreztorc ~/Desktop/Tech/Projects/Setup/Configs/nixUserHome
+cp -R ~/.zprezto ~/Desktop/Tech/Projects/Setup/Configs/nixUserHome
+cp -R ~/.zplug ~/Desktop/Tech/Projects/Setup/Configs/nixUserHome
+cp -R ~/.p10k.zsh ~/Desktop/Tech/Projects/Setup/Configs/nixUserHome
+cp -R ~/.vscode ~/Desktop/Tech/Projects/Setup/Configs/nixUserHome
+cp -R ~/.config/lsd ~/Desktop/Tech/Projects/Setup/Configs/nixUserHome/.config
